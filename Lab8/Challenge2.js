7b69e232f05b451fc2545a76bf74696bb5010476
ds19zo <90408593+ds19zo@users.noreply.github.com>
2021-11-04 15:41:27 -0400

    update

--- Lab8/Challenge2.js
+++ Lab8/Challenge2.js
@@ -6,9 +6,9 @@ document.getElementById("red").style.color="red";
 //A function to change colours of elements when called
 function mix(){
 
-	var first="#00BB00";
-	var second="#BB9320";
-	var third="#AB00FF";
+	var first ="#00BB00";
+	var second ="#BB9320";
+	var third ="#AB00FF";
 
 	document.getElementById("blue").style.color=first;
 	document.getElementById("green").style.color=second;
