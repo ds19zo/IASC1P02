0f4c256b7abc2cb92db19694d9b88cb2b52d6768
ds19zo <90408593+ds19zo@users.noreply.github.com>
2021-11-21 19:54:59 -0500

    Lab 10

--- Lab10/Java.js
+++ Lab10/Java.js
@@ -1,6 +1,27 @@
 function Add(){
-  var first = document.getElementById("Num1");
-  var second = document.getElementById("Num2");
+  var first = +(document.getElementById("Num1").value);
+  var second = +(document.getElementById("Num2").value);
   var output = first + second;
+  document.getElementById("output").innerHTML = output;
+}
+
+function Subtract(){
+  var first = +(document.getElementById("Num1").value);
+  var second = +(document.getElementById("Num2").value);
+  var output = first - second;
+  document.getElementById("output").innerHTML = output;
+}
+
+function Multiply(){
+  var first = +(document.getElementById("Num1").value);
+  var second = +(document.getElementById("Num2").value);
+  var output = first * second;
+  document.getElementById("output").innerHTML = output;
+}
+
+function Divide(){
+  var first = +(document.getElementById("Num1").value);
+  var second = +(document.getElementById("Num2").value);
+  var output = first / second;
   document.getElementById("output").innerHTML = output;
 }
